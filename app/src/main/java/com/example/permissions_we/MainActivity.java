7a05ad92bfbc3f036c345487156ba25c45b21f36
kunalcodes <kunal.chaturvedi75@gmail.com>
2021-07-06 16:46:08 +0530

    Files Added

---
+++ app/src/main/java/com/example/permissions_we/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.permissions_we;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnRequestPermission;
+    private static final int REQUEST_CODE = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mBtnRequestPermission = findViewById(R.id.btnRequestPermission);
+
+        mBtnRequestPermission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE};
+                ActivityCompat.requestPermissions(MainActivity.this, permissions, REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,  int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+            showToast("Camera and Storage Permission granted");
+        } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_DENIED){
+            showToast("Camera granted but storage denied");
+        } else if (grantResults[0] == PackageManager.PERMISSION_DENIED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+            showToast("Camera is denied but storage granted");
+        } else if (grantResults[0] == PackageManager.PERMISSION_DENIED && grantResults[1] == PackageManager.PERMISSION_DENIED){
+            showToast("Both permissions are denied");
+        }
+
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(MainActivity.this, message,Toast.LENGTH_SHORT).show();
+    }
+}
